7f929ced4dd0232563ca029f502d7bf4b77a266f
D. Adamson <0xwebmilk@gmail.com>
2024-08-10 22:53:27 +0300

    Some changes

--- src/core/routers/inline/append_order_router.py
+++ src/core/routers/inline/append_order_router.py
@@ -1,4 +1,3 @@
-import asyncio
 from aiogram import Router, F
 from aiogram.types import CallbackQuery
 from aiogram.filters import StateFilter
@@ -9,6 +8,6 @@ from core.fsm.AppendOrder import AppendOrder
 router = Router()
 
 @router.callback_query(StateFilter(None), F.data == "append_order")
-async def append_order(callback: CallbackQuery, state: FSMContext):
+async def append_order_handler(callback: CallbackQuery, state: FSMContext):
     await callback.message.edit_text(text='Введите номер телефона')
     await state.set_state(AppendOrder.enter_phone_number)
