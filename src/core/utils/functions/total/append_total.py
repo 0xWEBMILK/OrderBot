ea152361fbd754c3aee3799bc50e43d07dba1036
D. Adamson <0xwebmilk@gmail.com>
2024-08-13 08:49:06 +0300

    Some changes

--- src/core/utils/functions/total/append_total.py
+++ src/core/utils/functions/total/append_total.py
@@ -1,6 +1,9 @@
 import json
 
-async def append_total(file_path: str = None, file_name: str = None, total: str = 0) -> None:
+async def append_total(file_path: str = None,
+                       file_name: str = None,
+                       total: str = 0) -> None:
+    
     with open(f'{file_path}/{file_name}') as file:
         data = json.load(file)
         data['total'] = total
