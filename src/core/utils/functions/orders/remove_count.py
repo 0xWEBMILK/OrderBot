ab986da165d4f7fdb0687001567fc98bcd37e825
D. Adamson <0xwebmilk@gmail.com>
2024-08-13 08:49:03 +0300

    Some changes

--- src/core/utils/functions/orders/remove_count.py
+++ src/core/utils/functions/orders/remove_count.py
@@ -1,6 +1,9 @@
 import json
 
-async def remove_count(file_path: str = None, file_name: str = None, total: str = 0) -> None:
+async def remove_count(file_path: str = None,
+                       file_name: str = None,
+                       total: str = 0) -> None:
+    
     with open(f'{file_path}/{file_name}') as file:
         data = json.load(file)
         data['temp'] = int(data['temp']) - int(total)
