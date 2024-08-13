88d6cee3d92c618ba50eb0e0a0297ddf3af68e49
D. Adamson <0xwebmilk@gmail.com>
2024-08-13 08:49:09 +0300

    Some changes

--- src/core/utils/functions/total/fetch_temp.py
+++ src/core/utils/functions/total/fetch_temp.py
@@ -1,5 +1,7 @@
 import json
 
-async def fetch_temp(file_path: str = None, file_name: str = None) -> str:
+async def fetch_temp(file_path: str = None,
+                     file_name: str = None) -> str:
+    
     with open(f'{file_path}/{file_name}') as file:
         return json.load(file)['temp']
